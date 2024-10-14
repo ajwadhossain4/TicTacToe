d151f031d053e3b812b43babb8acc1a9efcd2fcb
ajwadhossain4 <ajwadhossain2003@gmail.com>
2024-10-14 21:49:44 +0600

    Plan

---
+++ XOXO.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//print the board
+//input moves
+//check if moves are valid
+//alternate between X and O
+//check if pattern is a win or a draw
+
+int main()
+{
+    char pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9;
+
+}
