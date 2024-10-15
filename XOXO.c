938d2a0d9c728714bf0bb380eecc6289f0ff2cdf
ajwadhossain4 <ajwadhossain2003@gmail.com>
2024-10-15 22:52:02 +0600

    Board Update

--- XOXO.c
+++ XOXO.c
@@ -1,13 +1,33 @@
 #include<stdio.h>
 
 //print the board
+    //assign characters to each position
+    //give the user an example and be minimalistic
+    //print the board each time a new move is given
 //input moves
+    //only X or O
 //check if moves are valid
+    //move valid for the given position
 //alternate between X and O
+    //users' inputs in a while loop
+    //since this is a game between two players taking a turn each
 //check if pattern is a win or a draw
+    //enter all possible win conditions, either X or O wins
+    //else it's a draw
 
+char c1, c2, c3, c4, c5, c6, c7, c8, c9;
+
+void XOXOBoard()
+{
+    printf(" %c | %c | %c \n", c1, c2, c3);
+    printf(" - + - + - \n");
+    printf(" %c | %c | %c \n", c4, c5, c6);
+    printf(" - + - + - \n");
+    printf(" %c | %c | %c \n", c7, c8, c9);
+}
 int main()
 {
-    char pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9;
-
+    c1 = c2 = c3 = c4 = c5 = c6 = c7 = c8 = c9 = ' ';
+    XOXOBoard();
+    return 0;
 }
